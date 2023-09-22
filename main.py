59f0c6ecf13ceb44ec9eea5eea8441ae48aa11a3
lzj1899 <145450814+lzj1899@users.noreply.github.com>
2023-09-22 12:59:48 +0800

    read textfile & csv

--- main.py
+++ main.py
@@ -2,10 +2,31 @@ import csv, json
 
 fileName = input("Please enter the name of the file you wish to open: ")
 
+def fileType(fileName):
+    length = len(fileName)
+    fileFormat = fileName[length - 4:]
+    return fileFormat
+
 def readText(fileName):
     with open(fileName) as data:
         content = data.read()
         return content
     
-contents = readText(fileName)
-print(contents)
+def readCSV(fileName):
+    with open(fileName, newline = '') as data:
+        content = csv.reader(data)
+        for row in content:
+            print(', '.join(row))
+
+fileFormat = fileType(fileName)
+
+if fileFormat == '.txt':
+    contents = readText(fileName)
+    contents += "\n I have edited this file"
+    print(contents)
+elif fileFormat == '.csv':
+    readCSV(fileName)
+elif fileFormat == 'json':
+    exit()
+else:
+    exit(print("Invalid file type"))
